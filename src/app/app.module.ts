33717d56ed32322f7443b9f5dbcabfb548772299
lucia-pitrelli <pitrelliblucia@gmail.com>
2023-02-23 02:28:51 -0300

    add data base json and new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProjectsComponent } from './components/projects/projects.component';
+import { ProjectComponent } from './components/project/project.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListProjectsComponent } from './components/list-projects/list-projects.component';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,11 @@ import { PresentationComponent } from './components/presentation/presentation.co
     AboutComponent,
     ContactComponent,
     HomeComponent,
-    ProjectsComponent,
+    ProjectComponent,
     HeaderComponent,
     PresentationComponent,
+    FooterComponent,
+    ListProjectsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
