d75e10a593d26f95dddf84db6ad9f40fe11dea46
lucia-pitrelli <pitrelliblucia@gmail.com>
2023-03-04 05:29:24 -0300

    add light and dark theme. Add css variables

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  //coloco modo oscuro
+  toggleDarkTheme(): void {
+    document.body.classList.toggle('dark-theme');
+  }
 }
