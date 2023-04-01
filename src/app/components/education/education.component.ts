e1cf7cc7c61320ec61cd8d7a8ba0dc3d2c90df78
lucia-pitrelli <pitrelliblucia@gmail.com>
2023-04-01 16:42:08 -0300

    changed data base, component education and styles

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.css']
+  styleUrls: ['./education.component.css'],
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit {
+  listofCourses: any;
 
+  constructor(private datosPortfolio: PortfolioService) {}
+
+  //suscribe para utilizar el data.json
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe((data) => {
+      console.log(data);
+      this.listofCourses = data.education; //entra a data.json y luego entra a el array projects para poder usar las variables de adentro
+    });
+  }
 }
