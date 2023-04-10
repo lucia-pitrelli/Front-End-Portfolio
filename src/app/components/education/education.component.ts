ed248869f276c3ca553e283a3604ae560d5736fd
lucia-pitrelli <pitrelliblucia@gmail.com>
2023-04-10 20:15:34 -0300

    added form, modal, validators and styles in component education

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-education',
@@ -7,7 +8,31 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
   styleUrls: ['./education.component.css'],
 })
 export class EducationComponent implements OnInit {
+  //json data
   listofCourses: any;
+
+  //modal btn hide
+  showadd!: boolean;
+  showupdate!: boolean;
+
+  //form education
+  formValueEducation = new FormGroup({
+    institution: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(40),
+    ]),
+    degree: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(40),
+    ]),
+    date: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+  });
 
   constructor(private datosPortfolio: PortfolioService) {}
 
@@ -17,5 +42,17 @@ export class EducationComponent implements OnInit {
       console.log(data);
       this.listofCourses = data.education; //entra a data.json y luego entra a el array projects para poder usar las variables de adentro
     });
+  }
+
+  //hide btn add
+  add() {
+    this.showadd = true;
+    this.showupdate = false;
+  }
+
+  //hide btn update
+  update() {
+    this.showadd = false;
+    this.showupdate = true;
   }
 }
