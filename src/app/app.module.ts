8cc87964c643e24a618819dd8a3af2e3f4a99c69
lucia-pitrelli <pitrelliblucia@gmail.com>
2023-04-22 05:14:51 -0300

    delete some modals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,33 @@ import { AppComponent } from './app.component';
 
 //components
 import { AboutComponent } from './components/about/about.component';
+
 import { ContactComponent } from './components/contact/contact.component';
+
 import { HomeComponent } from './components/home/home.component';
 
 import { HeaderComponent } from './components/header/header.component';
+
 import { PresentationComponent } from './components/presentation/presentation.component';
+
 import { FooterComponent } from './components/footer/footer.component';
+
 import { ListProjectsComponent } from './components/list-projects/list-projects.component';
+
 import { LoginComponent } from './components/login/login.component';
+
 import { ChangeProjectComponent } from './components/modals/change-project/change-project.component';
+
 import { AddProjectComponent } from './components/modals/add-project/add-project.component';
+
 import { DeleteProjectComponent } from './components/modals/delete-project/delete-project.component';
+
 import { SkillsComponent } from './components/skills/skills.component';
+
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
 import { EducationComponent } from './components/education/education.component';
 
-import { AddAboutComponent } from './components/modals/add-about/add-about.component';
-
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ButtonLoginComponent } from './components/button-login/button-login.component';
 import { ButtonLogoutComponent } from './components/button-logout/button-logout.component';
 //import { environment } from 'src/environments/environment';
@@ -39,7 +48,6 @@ import { ButtonLogoutComponent } from './components/button-logout/button-logout.
     AboutComponent,
     ContactComponent,
     HomeComponent,
-
     HeaderComponent,
     PresentationComponent,
     FooterComponent,
@@ -51,8 +59,6 @@ import { ButtonLogoutComponent } from './components/button-logout/button-logout.
     SkillsComponent,
     PageNotFoundComponent,
     EducationComponent,
-    AddAboutComponent,
-    DashboardComponent,
     ButtonLoginComponent,
     ButtonLogoutComponent,
   ],
